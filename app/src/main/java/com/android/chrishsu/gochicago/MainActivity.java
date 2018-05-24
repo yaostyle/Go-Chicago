dc2eb0ae8c5f807997faa4f4b1ee6b65ac52f31c
yaostyle <yaostyle@gmail.com>
2018-05-23 18:49:19 -0700

    Initial commit

---
+++ app/src/main/java/com/android/chrishsu/gochicago/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.chrishsu.gochicago;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int actionBarTitleId = Resources.getSystem().getIdentifier("action_bar_title", "id", "android");
+        if (actionBarTitleId > 0) {
+            TextView title = (TextView) findViewById(actionBarTitleId);
+            if (title != null) {
+                title.setTextColor(Color.RED);
+            }
+        }
+    }
+}
